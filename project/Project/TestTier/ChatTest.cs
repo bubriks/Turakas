88fbf8a3ab19739fad126a84c51176786870e752
bubriks <bubriks@gmail.com>
2017-10-24 14:51:17 +0200

    Added tests but transaction has to revieved

---
+++ project/Project/TestTier/ChatTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessTier;
+
+namespace TestTier
+{
+    [TestClass]
+    public class ChatTest
+    {
+        ChatController controller = new ChatController();
+
+        [TestMethod]
+        public void TestGetChatMethod()
+        {
+            Assert.AreNotEqual(null, controller.GetChat(1));
+        }
+    }
+}
---
+++ project/Project/TestTier/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestTier")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("339b64cb-bb56-434e-aca1-6b1328864a64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
