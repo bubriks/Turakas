9f0a551b4cf3f214d6d28a40eb0a1df5ad5e082e
Topki911 <gamingpills@abv.bg>
2017-10-29 23:57:26 +0100

    DBSong - AddSong method

--- project/Project/DataAccessTier/DBSong.cs
+++ project/Project/DataAccessTier/DBSong.cs
@@ -1,7 +1,25 @@
-﻿namespace DataAccessTier
+﻿using System.Data.SqlClient;
+
+namespace DataAccessTier
 {
     public class DBSong
     {
-        
+        private SqlConnection con = null;
+        public DBSong()
+        {
+            con = DbConnection.GetInstance().GetConnection();
+        }
+        public void AddSong(int activityID, int artistID, int genreID, string name, int duration, string url)
+        {
+            string stmt = "INSERT INTO Song(activityID, artistID, genreID, name, duration, url) values (@0, @1, @2, @3, @4, @5)";
+            SqlCommand cmd = new SqlCommand(stmt, con);
+            cmd.Parameters.AddWithValue("@0", activityID);
+            cmd.Parameters.AddWithValue("@1", artistID);
+            cmd.Parameters.AddWithValue("@2", genreID);
+            cmd.Parameters.AddWithValue("@3", name);
+            cmd.Parameters.AddWithValue("@4", duration);
+            cmd.Parameters.AddWithValue("@5", url);
+            cmd.ExecuteNonQuery();
+        }
     }
 }
