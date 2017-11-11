fa99bd92bb88c7282df4dc96f389a7034016bfb4
EstTurakas <EstTurakas@users.noreply.github.com>
2017-11-12 00:22:44 +0200

    Add files via upload

---
+++ project/Project/PresentationTier/ChatUI/ChatUI/ChatUI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatUI
+{
+    public partial class ChatUI : Form
+    {
+        public ChatUI()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInvite_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
