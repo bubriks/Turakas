aa5ebc67d0a6b7dbb99a0f260ab8a18159a382dd
Topki911 <gamingpills@abv.bg>
2017-11-21 19:42:03 +0100

    C# causes cancer final stage only

--- project/Project/PresentationTier/Program.cs
+++ project/Project/PresentationTier/Program.cs
@@ -16,7 +16,7 @@ namespace PresentationTier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ChatForm(2));
+            Application.Run(new YoutubeAlpha());
         }
     }
 }
--- project/Project/PresentationTier/YoutubeAlpha.cs
+++ project/Project/PresentationTier/YoutubeAlpha.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace PresentationTier
 {
@@ -16,13 +18,15 @@ namespace PresentationTier
         private YoutubeService.YoutubeServiceClient youtubeServiceClient = new YoutubeService.YoutubeServiceClient();
         public YoutubeAlpha()
         {
+            SetBrowserFeatureControl();
+            
             InitializeComponent();
             
         }
 
         string ytUrl;
 
-        public string VideoId
+        private string VideoId
         {
             get
             {
@@ -30,6 +34,8 @@ namespace PresentationTier
                 return ytMatch.Success ? ytMatch.Groups[1].Value : string.Empty;
             }
         }
+        
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -38,18 +44,98 @@ namespace PresentationTier
             label1.Text = vidTitle;
             //Console.WriteLine(VideoId);
             //Console.ReadLine();
-            webBrowser1.Navigate($"https//www.youtube.com/watch?v={VideoId}");
-            /*
-            var page = $@"
-        <html>
-            <body>
-                <iframe width='560' height='315'
-                    src='https//www.youtube.com/embed/{VideoId}' frameborder='0'>
-                </iframe>
-            </body>
-        <html>";
-            webBrowser1.DocumentText = page;
-            webBrowser1.ScriptErrorsSuppressed = true;*/
+            //webBrowser1.Navigate($"https://www.youtube.com/watch?v={VideoId}");
+            //webBrowser1.Navigate($"https://www.youtube.com/embed{VideoId}");
+            
+            const string page1 = "<html><head><title></title></head><body>{0}</body></html>";
+            webBrowser1.DocumentText = string.Format(page1, $"<iframe width=\"560\" height=\"315\" src=\"http://www.youtube.com/embed/{VideoId}?autoplay=1\" frameborder=\"0\" allowfullscreen></iframe>");
+        }
+        
+        private void SetBrowserFeatureControlKey(string feature, string appName, uint value)
+        {
+            using (var key = Registry.CurrentUser.CreateSubKey(
+                String.Concat(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\", feature), 
+                RegistryKeyPermissionCheck.ReadWriteSubTree))
+            {
+                key.SetValue(appName, (UInt32)value, RegistryValueKind.DWord);
+            }
+        }
+        
+        private void SetBrowserFeatureControl()
+        {
+            // http://msdn.microsoft.com/en-us/library/ee330720(v=vs.85).aspx
+
+            // FeatureControl settings are per-process
+            var fileName = System.IO.Path.GetFileName(Process.GetCurrentProcess().MainModule.FileName);
+
+            // make sure the control is not running inside Visual Studio Designer
+            if (String.Compare(fileName, "devenv.exe", true) == 0 || String.Compare(fileName, "XDesProc.exe", true) == 0) 
+            return;
+
+            SetBrowserFeatureControlKey("FEATURE_BROWSER_EMULATION", fileName, GetBrowserEmulationMode()); // Webpages containing standards-based !DOCTYPE directives are displayed in IE10 Standards mode.
+            SetBrowserFeatureControlKey("FEATURE_AJAX_CONNECTIONEVENTS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ENABLE_CLIPCHILDREN_OPTIMIZATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_MANAGE_SCRIPT_CIRCULAR_REFS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_DOMSTORAGE ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_GPU_RENDERING ", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_IVIEWOBJECTDRAW_DMLT9_WITH_GDI  ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_LEGACY_COMPRESSION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_LOCALMACHINE_LOCKDOWN", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_OBJECT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_BLOCK_LMZ_SCRIPT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_DISABLE_NAVIGATION_SOUNDS", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SCRIPTURL_MITIGATION", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_SPELLCHECKING", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_STATUS_BAR_THROTTLING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_TABBED_BROWSING", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_VALIDATE_NAVIGATE_URL", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_DOCUMENT_ZOOM", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_POPUPMANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBOC_MOVESIZECHILD", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_ADDON_MANAGEMENT", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_WEBSOCKET", fileName, 1);
+            SetBrowserFeatureControlKey("FEATURE_WINDOW_RESTRICTIONS ", fileName, 0);
+            SetBrowserFeatureControlKey("FEATURE_XMLHTTP", fileName, 1);
+        }
+
+        private UInt32 GetBrowserEmulationMode()
+        {
+            int browserVersion = 7;
+            using (var ieKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer",
+            RegistryKeyPermissionCheck.ReadSubTree,
+            System.Security.AccessControl.RegistryRights.QueryValues))
+            {
+                var version = ieKey.GetValue("svcVersion");
+                if (null == version)
+                {
+                    version = ieKey.GetValue("Version");
+                    if (null == version)
+                    throw new ApplicationException("Microsoft Internet Explorer is required!");
+                }
+            int.TryParse(version.ToString().Split('.')[0], out browserVersion);
+            }
+    
+            UInt32 mode = 11000; // Internet Explorer 11. Webpages containing standards-based !DOCTYPE directives are displayed in IE11 Standards mode. Default value for Internet Explorer 11.
+            switch (browserVersion)
+            {
+            case 7:
+                mode = 7000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE7 Standards mode. Default value for applications hosting the WebBrowser Control.
+                break;
+            case 8:
+                mode = 8000; // Webpages containing standards-based !DOCTYPE directives are displayed in IE8 mode. Default value for Internet Explorer 8
+                break;
+            case 9:
+                mode = 9000; // Internet Explorer 9. Webpages containing standards-based !DOCTYPE directives are displayed in IE9 mode. Default value for Internet Explorer 9.
+                break;
+            case 10:
+                mode = 10000; // Internet Explorer 10. Webpages containing standards-based !DOCTYPE directives are displayed in IE10 mode. Default value for Internet Explorer 10.
+                break;
+            default:
+                // use IE11 mode by default
+                break;
+            }
+    
+        return mode;
         }
     }
 }
