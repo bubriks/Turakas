90e4fe7f67630d9641d8749f8f2cb1b19227b927
Stoycho Anastasov Nenov <1062073@ucn.dk>
2017-11-27 11:25:31 +0100

    microsoft can go to hell

--- project/Project/BusinessTier/Interfaces/IPlayListController.cs
+++ project/Project/BusinessTier/Interfaces/IPlayListController.cs
@@ -1,7 +1,17 @@
-﻿namespace BusinessTier.Interfaces
+﻿using DataTier;
+using System.Collections.Generic;
+
+namespace BusinessTier.Interfaces
 {
     public interface IPlayListController
     {
-        
+        void AddPlayList(string name);
+        List<PlayList> FindPlayListsByName(string name);
+        bool AddSongToPlayList(int songId, int playListId);
+
+        List<Song> GetSongsFromPlayList(int playListId);
+
+
+
     }
 }
