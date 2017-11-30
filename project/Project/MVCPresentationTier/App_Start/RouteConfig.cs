ee79247c371caad950249b430016156307d1f094
bubriks <bubriks@gmail.com>
2017-11-30 15:58:25 +0100

    mvc chat update

--- project/Project/MVCPresentationTier/App_Start/RouteConfig.cs
+++ project/Project/MVCPresentationTier/App_Start/RouteConfig.cs
@@ -14,6 +14,18 @@ namespace MVCPresentationTier
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "ChatRoom",
+                "Message/ChatRoom/{chatId}/{profileId}",
+                new { controller = "Message", action = "ChatRoom", chatId = UrlParameter.Optional, profileId = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                "GetChat",
+                "Chat/GetChat/{chat}/",
+                new { controller = "Chat", action = "GetChat", chat = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 "GetChats",
                 "Chat/GetChats/{input}/{profileId}",
                 new { controller = "Chat", action = "GetChats", input = UrlParameter.Optional , profileId = UrlParameter.Optional }
