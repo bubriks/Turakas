100bd8093c05e850fa19312ff814361cfd9f93b9
bubriks <bubriks@gmail.com>
2017-12-03 13:31:49 +0100

    mvc chat kinda works, maybe possible to make it work connected to wcf

--- project/Project/MVCPresentationTier/Hubs/ChatHub.cs
+++ project/Project/MVCPresentationTier/Hubs/ChatHub.cs
@@ -7,10 +7,16 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string groupName, string name, string message)
         {
-            // Call the addNewMessageToPage method to update clients.
-            Clients.All.addNewMessageToPage(name, message);
+            // Call the addNewMessageToPage method to update clients in group.
+            Clients.Group(groupName).addChatMessage(name, message);
+        }
+
+        public async Task JoinRoom(string roomName, string name)
+        {
+            await Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage(name, " joined.");
         }
     }
 }
