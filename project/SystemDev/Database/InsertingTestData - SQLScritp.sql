2c1ae6943ce990c56af884bb8b6b71e85bf3b102
RaidenRabit <1062021@ucn.dk>
2017-12-14 16:51:28 +0100

    small fixes to cascade delete

--- project/SystemDev/Database/InsertingTestData - SQLScritp.sql
+++ project/SystemDev/Database/InsertingTestData - SQLScritp.sql
@@ -1,4 +1,4 @@
-use dmaj0916_197331
+use dmaj0916_197331;
 
 --inserting Uganda 123456 and hanes 123456 in Profile
 DECLARE @salt UNIQUEIDENTIFIER=NEWID() INSERT INTO Profile(username, salt, passwordHash, email, nickname)OUTPUT INSERTED.profileID values ('Uganda', @salt, HASHBYTES('SHA2_512', '123456'+CAST(@salt AS NVARCHAR(36))), '959s9f+7w0g77dvgwrb0@sharklasers.com', 'Uganda' );
@@ -9,3 +9,13 @@ insert into Activity(profileID, timeStamp) values(2, '2016-01-01 09:40:30');
 insert into Groups(activityID, name) values(1, 'testGroup');
 insert into GroupMembers(activityID, groupID)  values(1, 1);
 insert into GroupMembers(activityID, groupID)  values(2, 1);
+--inserting testChatUgandaPr testChatHanesPu in Chat and Activity
+insert into Activity(profileId, timeStamp) values(1, '2016-01-01 09:40:30');
+insert into Activity(profileId, timeStamp) values(2, '2016-01-01 09:40:30');
+insert into Chat(activityID, name, type, nrOfUsers) values(3, 'testChatUgandaPr', 0, 10);
+insert into Chat(activityID, name, type, nrOfUsers) values(4, 'testChatHanesPu', 1, 15);
+--inserting messages into testChatUgandaPr and Activity
+insert into Activity(profileId, timeStamp) values(1, '2016-01-01 09:40:30');
+insert into Activity(profileId, timeStamp) values(2, '2016-01-01 09:40:30');
+insert into Message(activityID, chatActivityID, message) values(5, 3, 'Hello, Hanes!');
+insert into Message(activityID, chatActivityID, message) values(6, 3, 'Hi, Uganda.');
